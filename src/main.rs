f01d1ce101c3272c3cdcd5b859fa396438800fe9
miyamoto-kaei <kaei199919@gmail.com>
2026-06-07 06:28:42 +0900

    ソースファイルを読み込める

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use clap::Parser;
+use std::{fs, path::PathBuf};
+
+mod simple_interpreter;
 /// brainfuckの構文木
 mod syntax_tree;
-mod simple_interpreter;
 
-fn main() {
-    let code = ">++++[<++++++++>-]>++++++++[<++++++>-]<++.<.>+.<.>++.<.>++.<.>------..<.>.++.<.>--.++++++.<.>------.>+++[<+++>-]<-.<.>-------.+.<.> -.+++++++.<.>------.--.<.>++.++++.<.>---.---.<.> +++.-.<.>+.+++.<.>--.--.<.> ++.++++.<.>---.-----.<.>+++++.+.<.>.------.<.> ++++++.----.<.> ++++.++.<.> -.-----.<.>+++++.+.<.>.--.";
-    let program = simple_interpreter::Program::new(code).unwrap();
-    program.run().unwrap();
+/// brainfuckインタプリタ
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// ソースファイルのパス
+    file: PathBuf,
 }
 
+fn main() {
+    let args = Args::parse();
+
+    let code = fs::read_to_string(args.file).unwrap();
+    let program = simple_interpreter::Program::new(&code).unwrap();
+    program.run().unwrap();
+}
